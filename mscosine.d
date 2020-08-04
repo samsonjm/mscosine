512c89d672c7233e0c0719d42c38163a24e2dd53
Jonathan Samson <jonathan.samson@wur.nl>
2020-08-04 14:16:30 +0200

    Implemented find_cosine_score.

--- mscosine.d
+++ mscosine.d
@@ -3,9 +3,11 @@
  * Author: Jonathan Samson
  * Date: 4 August 2020
  */
+module mscosine;
 import std.algorithm;
 import cosine;
-import scans;
+import std.math;
+import std.exception;
 
 real[] combine_peak_lists(real[] mz1, real[] mz2)
 /* Creates a combined list of peaks from the separate peak lists.
@@ -57,7 +59,6 @@ real[real][2] create_vectors(
 	real[] first_peaks = first_scan.keys();
 	real[] second_peaks = second_scan.keys();
 	real[] peak_list = combine_peak_lists(first_peaks, second_peaks);
-
 	real[real] first_vector;
 	real[real] second_vector;
 	sort(first_peaks);
@@ -65,20 +66,25 @@ real[real][2] create_vectors(
 	real[] to_pop;
 	while(peak_list.length > 0)
 	{
+		real total_value = 0;
 		while(first_peaks.length > 0 && 
 		      peak_list[0] + threshold >= first_peaks[0])
 		{
 			to_pop ~= first_peaks[0];
-			first_vector[peak_list[0]] = first_scan[first_peaks[0]];
+			total_value += first_scan[first_peaks[0]];
 			first_peaks = first_peaks[1..$];
 		}
+		first_vector[peak_list[0]] = total_value;
+		total_value = 0;
 		while(second_peaks.length > 0 &&
 		      peak_list[0] + threshold >= second_peaks[0])
 		{
 			to_pop ~= second_peaks[0];
-			second_vector[peak_list[0]] = second_scan[second_peaks[0]];
+			total_value += second_scan[second_peaks[0]];
 			second_peaks = second_peaks[1..$];
 		}
+		second_vector[peak_list[0]] = total_value;
+		sort(to_pop);
 		to_pop.length -= to_pop.uniq().copy(to_pop).length;
 		peak_list = peak_list[(to_pop.length)..$];
 		to_pop = [];
@@ -90,14 +96,85 @@ unittest
 {
 	real[real] scan1_peaks = [
 		100.1: 10000.100,
+		100.2: 500,
 		200.1: 15000.2,
 		300.1: 16000.3
 	];
 	real[real] scan2_peaks = [
-		100.1: 10000.100,
-		200.1: 15000.2,
-		300.1: 16000.3
+		100.1: 10000.1,
+		200.2: 15000.2,
+		300.3: 16000.3
 	];
-	assert(create_vectors(scan1_peaks, scan2_peaks) == 
-			[scan1_peaks, scan2_peaks]);
+	real[real] scan1_vector = [
+		100.1: 10500.100,
+		200.1: 15000.2,
+		300.1: 16000.3,
+		300.3: 0 
+	];
+	real[real] scan2_vector = [
+		100.1: 10000.1,
+		200.1: 15000.2,
+		300.1: 0,
+		300.3: 16000.3
+	];
+	assert(create_vectors(scan1_peaks, scan2_peaks, 0.1) == [
+		scan1_vector, scan2_vector]);
+}
+
+bool not_all_zeroes(real[] list)
+/* Checks whether all values are 0.
+ * Arguments:
+ *	list - A list of real values.
+ * Returns:
+ *	true - If at least one value in list is not 0.
+ *	false - If all values in list are 0.
+ */
+{
+	foreach(real value; list)
+	{
+		if(value != 0)
+			return true;
+	}
+	return false;
+}
+unittest
+{
+	real[] test = [0, 0.0, 0.000000];
+	assert(not_all_zeroes(test) == false);
+	test = [0, 0.0, 0.00000001];
+	assert(not_all_zeroes(test) == true);
+}
+
+real find_cosine_score(
+		real[real] first_scan, 
+		real[real] second_scan, 
+		real threshold=0.01)
+/* Calculates the cosine score between the two scans.
+ * Arguments:
+ *	first_scan - scan with mz:intensity.
+ *	second_scan - scan with mz:intensity.
+ *	threshold - mz tolerance to be considered the same peak.
+ * Returns:
+ *	score - the cosine score bewteen the two scans.
+ */
+{
+	enforce(not_all_zeroes(first_scan.values),
+			"The first scan is all 0, cosine is NaN");
+	enforce(not_all_zeroes(second_scan.values),
+			"The second scan is all 0, cosine is NaN");
+	real[real][2] vectors = create_vectors(first_scan, second_scan, threshold);
+	real[] first_scores = vectors[0].values;
+	real[] second_scores = vectors[1].values;
+	real score = calculate_cosine_score(first_scores, second_scores);
+	return score;
+}
+unittest
+{
+	real[real] scan1 = [1: 2000, 2:3000, 3:1000];
+	real[real] scan2 = [1: 2000, 2:3000, 3:1000];
+	assert(approxEqual(find_cosine_score(scan1, scan2), 1));
+	scan2 = [1: -2000, 2:-3000, 3:-1000];
+	assert(approxEqual(find_cosine_score(scan1, scan2), -1));
+	scan2 = [1:0];
+	assertThrown(find_cosine_score(scan1, scan2));
 }
